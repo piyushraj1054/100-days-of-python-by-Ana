83afcf71dcfc62a59a5ef3ec4864c773793d1250
Piyush <piyushraj1054@gmail.com>
2023-12-26 11:36:53 +0530

    String concatination day 1

---
+++ Code/main.py
@@ -0,0 +1,9 @@
+# Printing my world first time
+print("Hello World!")
+print ("Day 1 - Puthon Print Function")
+print("The function is declared like this:")
+print("print('What to print')")
+print ("Hello world! \nHello world!\nHello world!")
+# Adding space using string concatination
+print ("Hello"+" World")
+print("Hello" + " " +"World!")
