eb70ad611628ae1b1cb50ee517ee9a368cdb2d93
Piyush <piyushraj1054@gmail.com>
2023-12-27 10:54:36 +0530

    Input function

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,9 @@
 # Printing my world first time
 print("Hello World!")
-print ("Day 1 - Puthon Print Function")
+print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('What to print')")
-print ("Hello world! \nHello world!\nHello world!")
+print("Hello world! \nHello world!\nHello world!")
 # Adding space using string concatination
-print ("Hello"+" World")
-print("Hello" + " " +"World!")
+print("Hello" + " World")
+print("Hello" + " " + "World!")
