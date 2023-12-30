8d36d20330d18118291a906906baced96a4c56a4
Piyush <piyushraj1054@gmail.com>
2023-12-30 12:07:20 +0530

    band name genrerator

---
+++ Code/Day-1/Variables.py
@@ -0,0 +1,13 @@
+#name = input("what is your name ?")
+#print(name)
+#print(len(name))
+# variable switch. switch value of a to and b to a
+a=10
+b=20
+print(a)
+print(b)
+c=a # c is 10
+a=b # a is 20
+b=c # b is 10
+print(a)
+print(b)
---
+++ Code/Day-1/band-name-generator.py
@@ -0,0 +1,8 @@
+ # greeting to the applicatio
+print("Welcome to the band name generator.")
+#Ask the city of birth
+city=input("Which city did you grow up in?")
+# Ask the user for their pet name.
+pet=input("What is the name of your pet ?")
+#combine the city and pet name to generate the band name.
+print("Your band name is :" + city + pet)
